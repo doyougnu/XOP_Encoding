a67107169e0f3d835a697ea3f5f8ed7954876876
doyougnu <jmy6342@gmail.com>
2017-03-04 17:46:30 -0800

    organization, updated codings, AVL ex 2 coded

---
+++ notes/auto/codings.el
@@ -0,0 +1,45 @@
+(TeX-add-style-hook
+ "codings"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt" "letterpaper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry"
+    "adjustbox")
+   (LaTeX-add-labels
+    "sec:org4c99545"
+    "sec:orgd9f9156"
+    "sec:org8a62c5b"
+    "sec:org74ac92a"
+    "sec:org96cc204"
+    "sec:org0f2d149"
+    "sec:org0b85806"
+    "sec:orgb8ef15e"
+    "sec:orgb869863"))
+ :latex)
+
