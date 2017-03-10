8dc9ced0b9e9218d26ac9aa666310dbc3d441d08
doyougnu <jmy6342@gmail.com>
2017-03-09 16:48:29 -0800

    adding data collection section, syntax fixes

--- notes/auto/codings.el
+++ notes/auto/codings.el
@@ -5,12 +5,12 @@
                      '(("article" "10pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,14 +32,15 @@
     "geometry"
     "adjustbox")
    (LaTeX-add-labels
-    "sec:org4e0034f"
-    "sec:org0d4f588"
-    "sec:orgbe0d9dc"
-    "sec:org1ee15f7"
-    "sec:orgc0bd108"
-    "sec:org41bae11"
-    "sec:org6cfb060"
-    "sec:orgbce9be6"
-    "sec:org8a4d359"))
+    "sec:org411c3f2"
+    "sec:orgecf5855"
+    "sec:org6ccb1e4"
+    "sec:org2f4906d"
+    "sec:org1bae7ce"
+    "sec:org328433e"
+    "sec:org57ca4c4"
+    "sec:org50c3a9c"
+    "sec:orga9e8ece"
+    "sec:org4c1f85e"))
  :latex)
 
