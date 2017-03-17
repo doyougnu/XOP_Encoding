9d2dc40991e3905af92cc8e8406baccd9060589b
doyougnu <jmy6342@gmail.com>
2017-03-16 17:14:24 -0700

    splitting data, better visuals

--- R/analysis.r
+++ R/analysis.r
@@ -1,17 +1,54 @@
 library(ggplot2)
 library(scales)
 library(dplyr)
+library(gridExtra)
+# for mosaic plots
+library(ggmosaic)
 
-df <- read.table(file="../data/encodings_raw_03042017.txt"
+setwd("/home/doyougnu/Research/XOP/XOP_Encoding/R/")
+# read in each data table
+dfwrd <- read.table(file="../data/encodings_word_03162017.txt"
                , header = TRUE
                , row.names = NULL)
-df$Explanation <- as.character(df$Explanation)
-dff <- df %>%
+
+dfppt <- read.table(file="../data/encodings_ppt_03162017.txt"
+               , header = TRUE
+               , row.names = NULL)
+
+# Convert Explanations to something usable, the ($) operator selects a column in a dataframe
+dfwrd$Explanation <- as.character(dfwrd$Explanation)
+dfppt$Explanation <- as.character(dfppt$Explanation)
+
+# Add type of file to each datasets
+dfwrd %>% mutate(fileType = "word")
+dfppt %>% mutate(fileType = "ppt")
+
+# Get Algorithm key out, the (%>%) operating is function composition from dplyr package
+dfwrd <- dfwrd %>%
+  mutate(Algorithm = substring(Explanation, 0, 2))
+dfppt <- dfppt %>%
   mutate(Algorithm = substring(Explanation, 0, 2))
 
-data <- dff %>%
-  filter(!(Explanation == "UN007" && Coder == "B") && !(Explanation == "UN007" && Coder == "B"))
+# Compose actual data set from subsets, rbind stacked datasets that share the same header
+dataStacked <- rbind(dfwrd, dfppt)
 
+# Remove readings made for comparison
+data <- dataStacked %>%
+  filter(!(Explanation == "UN007" && Coder == "B"), !(Explanation == "UN007" && Coder == "B"))
+
+# Extract out the subset of rows where there are two coders, I've hardcoded this for now
+dfCoderComp <- dataStacked %>%
+  filter(Explanation == "UN007" | Explanation == "UN006")
+
+# Generate a data set that does not consider notations
+dfRole <- dataStacked %>%
+  select(-(Notation:isPartial:isEmphasized:hasMany)) %>%
+  distinct()
+
+# mosaic plot df
+mdf <- data %>% group_by(Explanation, Goal, Role, Notation, Position) %>% summarise(count = n())
+
+# Basic bar plot showing the percentages of roles per algorithm
 plt <- ggplot(data, aes(x=Role, fill=Role)) +
   geom_bar(colour="black", aes(y = (..count..)/sum(..count..))) +
   facet_grid(Algorithm ~ .) +
@@ -21,12 +58,10 @@ plt <- ggplot(data, aes(x=Role, fill=Role)) +
   guides(fill=FALSE)
 
 plt
-ggsave(file = "/home/doyougnu/Research/XOP_Encoding/R/Plots/RolePercentages.png", width = 7, height = 5)
+ggsave(file = "Plots/RolePercentages.png", width = 7, height = 5)
 
-comparisonDf <- dff %>%
-  filter(Explanation == "UN007" | Explanation == "UN006")
-
-plt2 <- ggplot(comparisonDf, aes(x=Role, fill=Role)) +
+# same plot just comparing each coder
+plt2 <- ggplot(dfCoderComp, aes(x=Role, fill=Role)) +
   geom_bar(colour="black", aes(y = (..count..)/sum(..count..))) +
   facet_grid(. ~ Coder) +
   scale_y_continuous(labels = percent) +
@@ -35,4 +70,39 @@ plt2 <- ggplot(comparisonDf, aes(x=Role, fill=Role)) +
   guides(fill=FALSE)
 
 plt2
-ggsave(file = "/home/doyougnu/Research/XOP_Encoding/R/Plots/RolePercentagesCoderComp.png", width = 7, height = 5)
+ggsave(file = "Plots/RolePercentagesCoderComp.png", width = 7, height = 5)
+
+#
+test <- data %>% group_by(Algorithm, Goal, Role, Notation) %>% summarise(count = n()) %>% arrange(desc(count))
+
+plt3 <- ggplot(test, aes(x=Role, y=count)) +
+  geom_bar(stat="identity") +
+  geom_bar(aes(x=Role, y=count, fill = Notation), stat="identity", position="dodge") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  facet_grid(.~ Algorithm)
+
+ggsave(file = "Plots/RoleNotationComposite.png", width = 7, height = 5)
+
+plt4 <- ggplot(dfRole, aes(x=Position, fill=Goal)) +
+  geom_bar(colour="black", aes(y = (..count..)/sum(..count..))) +
+  scale_y_continuous(labels = percent)
+plt4
+ggsave(file = "Plots/GoalsbyPosition.png", width = 7, height = 5)
+
+## Birds eye view, really need a mosaic plot
+plt5 <- ggplot(dfRole, aes(x=Position, fill=Role)) +
+  geom_bar(colour="black", aes(y = (..count..)/sum(..count..))) +
+  scale_y_continuous(labels = percent) +
+  facet_grid (Algorithm ~ Goal)
+plt5
+ggsave(file = "Plots/BirdsEyeView.png", width = 7, height = 5)
+
+## sample spine and mosaic plots
+plt6 <- plot(data$Role, data$Goal, xlab = "Roles", ylab = "Goals")
+plt6
+ggsave(file = "Plots/RoleGoalMosaic.png", width = 7, height = 5)
+
+
+## generate a contingecy table
+cNGtable <- table(data$Notation, data$Goal)
+cRGtable <- table(data$Role, data$Goal)
--- notes/auto/codings.el
+++ notes/auto/codings.el
@@ -5,12 +5,12 @@
                      '(("article" "10pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,15 +32,15 @@
     "geometry"
     "adjustbox")
    (LaTeX-add-labels
-    "sec:org411c3f2"
-    "sec:orgecf5855"
-    "sec:org6ccb1e4"
-    "sec:org2f4906d"
-    "sec:org1bae7ce"
-    "sec:org328433e"
-    "sec:org57ca4c4"
-    "sec:org50c3a9c"
-    "sec:orga9e8ece"
-    "sec:org4c1f85e"))
+    "sec:org512d8bf"
+    "sec:org0e48321"
+    "sec:org11045cc"
+    "sec:orga8bfcd0"
+    "sec:orge7e70f7"
+    "sec:org81b499e"
+    "sec:org48105f9"
+    "sec:orgc8fd49c"
+    "sec:org81b7ef7"
+    "sec:org7db075e"))
  :latex)
 
