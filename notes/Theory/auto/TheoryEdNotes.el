cea544eafedb5eaebd71fc5d412b3f20669bb8f7
doyougnu <jmy6342@gmail.com>
2017-03-30 11:11:55 -0700

    more notes

---
+++ notes/Theory/auto/TheoryEdNotes.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "TheoryEdNotes"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "brown1984explaining"
+    "Smith1970-SMIASO-13"
+    "smith1967language"
+    "bellack1966language"
+    "ennis1969logic"
+    "hyman1968teaching"
+    "thyne1965psychology"
+    "10.2307/3120386"))
+ :bibtex)
+
