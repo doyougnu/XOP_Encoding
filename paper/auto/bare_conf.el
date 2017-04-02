16b467fc0c674a876592d30fb63a4288e79bacae
doyougnu <jmy6342@gmail.com>
2017-04-01 18:12:53 -0700

    first stab at abstracts

---
+++ paper/auto/bare_conf.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "bare_conf"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("IEEEtran" "conference")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "IEEEtran"
+    "IEEEtran10")
+   (LaTeX-add-bibitems
+    "IEEEhowto:kopka"))
+ :latex)
+
