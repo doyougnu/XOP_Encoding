af08b65e9b5812c05f3af6672b733d4bbbf539ef
doyougnu <jmy6342@gmail.com>
2017-04-08 17:17:15 -0700

    symantic fixes

--- notes/auto/codings.el
+++ notes/auto/codings.el
@@ -5,12 +5,12 @@
                      '(("article" "10pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,27 +32,27 @@
     "geometry"
     "adjustbox")
    (LaTeX-add-labels
-    "sec:org4a7044d"
-    "sec:orga71438d"
-    "sec:org6ba551f"
-    "sec:org46b19b6"
-    "sec:orgcaa2f5b"
-    "sec:org1759d46"
-    "sec:orgd0f16eb"
-    "sec:orge1d42a6"
-    "sec:org6412cf7"
-    "sec:org0dbc9f3"
-    "sec:orgeee69bd"
-    "sec:org5b3cade"
-    "sec:orgf1c5cfa"
-    "sec:org84499b3"
-    "sec:org5dd1a10"
-    "sec:orgad5b425"
-    "sec:org2f6906d"
-    "sec:org9c8578b"
-    "sec:orgb4e3638"
-    "sec:orgaaac6ca"
-    "sec:orga13d218"
-    "sec:org58cd3dd"))
+    "sec:org2a22781"
+    "sec:org14a4dc2"
+    "sec:orgcb0dfcd"
+    "sec:orgd647f84"
+    "sec:org7fc34d1"
+    "sec:org57f3fbf"
+    "sec:org58c161c"
+    "sec:org8a9eb98"
+    "sec:org8327f3a"
+    "sec:org43afd12"
+    "sec:orge065711"
+    "sec:orgae6911c"
+    "sec:org9eb4349"
+    "sec:org527f03d"
+    "sec:orgc7994b8"
+    "sec:orgcc9fa38"
+    "sec:orgd751d63"
+    "sec:org37c9ed3"
+    "sec:orgfd0af08"
+    "sec:orgf4f0f6e"
+    "sec:orgb738125"
+    "sec:orga886580"))
  :latex)
 
