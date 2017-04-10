a82689bee154c86722f3cc1e04ebf443a71644a2
doyougnu <jmy6342@gmail.com>
2017-04-09 19:19:15 -0700

    adding substantive meaning categories for AVL and Dijkstras

--- notes/auto/codings.el
+++ notes/auto/codings.el
@@ -5,12 +5,12 @@
                      '(("article" "10pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,27 +32,30 @@
     "geometry"
     "adjustbox")
    (LaTeX-add-labels
-    "sec:org2a22781"
-    "sec:org14a4dc2"
-    "sec:orgcb0dfcd"
-    "sec:orgd647f84"
-    "sec:org7fc34d1"
-    "sec:org57f3fbf"
-    "sec:org58c161c"
-    "sec:org8a9eb98"
-    "sec:org8327f3a"
-    "sec:org43afd12"
-    "sec:orge065711"
-    "sec:orgae6911c"
-    "sec:org9eb4349"
-    "sec:org527f03d"
-    "sec:orgc7994b8"
-    "sec:orgcc9fa38"
-    "sec:orgd751d63"
-    "sec:org37c9ed3"
-    "sec:orgfd0af08"
-    "sec:orgf4f0f6e"
-    "sec:orgb738125"
-    "sec:orga886580"))
+    "sec:orgcf9217e"
+    "sec:org09c6723"
+    "sec:org348d146"
+    "sec:orgc4b6ce5"
+    "sec:org55e5be5"
+    "sec:orgfaba411"
+    "sec:orgdd1f5d9"
+    "sec:orgcd142aa"
+    "sec:org8c44917"
+    "sec:org4a017b2"
+    "sec:orge7520ab"
+    "sec:org0ee547d"
+    "sec:orge1c6f93"
+    "sec:org2fed38c"
+    "sec:org553f282"
+    "sec:org6f880a2"
+    "sec:org0ac2509"
+    "sec:org084d708"
+    "sec:org5468693"
+    "sec:orgf845279"
+    "sec:org79736e4"
+    "sec:org891e6b9"
+    "sec:orge9f1d47"
+    "sec:org0417910"
+    "sec:org125b204"))
  :latex)
 
