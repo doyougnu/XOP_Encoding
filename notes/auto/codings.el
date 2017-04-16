55a83208c0ab18a1c68ce485de4f535991fb5909
doyougnu <jmy6342@gmail.com>
2017-04-16 16:28:10 -0700

    Updated sources

--- notes/auto/codings.el
+++ notes/auto/codings.el
@@ -5,12 +5,12 @@
                      '(("article" "10pt" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,30 +32,31 @@
     "geometry"
     "adjustbox")
    (LaTeX-add-labels
-    "sec:orgcf9217e"
-    "sec:org09c6723"
-    "sec:org348d146"
-    "sec:orgc4b6ce5"
-    "sec:org55e5be5"
-    "sec:orgfaba411"
-    "sec:orgdd1f5d9"
-    "sec:orgcd142aa"
-    "sec:org8c44917"
-    "sec:org4a017b2"
-    "sec:orge7520ab"
-    "sec:org0ee547d"
-    "sec:orge1c6f93"
-    "sec:org2fed38c"
-    "sec:org553f282"
-    "sec:org6f880a2"
-    "sec:org0ac2509"
-    "sec:org084d708"
-    "sec:org5468693"
-    "sec:orgf845279"
-    "sec:org79736e4"
-    "sec:org891e6b9"
-    "sec:orge9f1d47"
-    "sec:org0417910"
-    "sec:org125b204"))
+    "sec:org304ba2b"
+    "sec:org1760a85"
+    "sec:orga066dcb"
+    "sec:org888abd0"
+    "sec:orgeef03b9"
+    "sec:org7a4a481"
+    "sec:org99d552a"
+    "sec:org3e41e11"
+    "sec:org1835e48"
+    "sec:org18364a5"
+    "sec:orgdb67230"
+    "sec:org497cd77"
+    "sec:orgc90aa15"
+    "sec:orgf269e0a"
+    "sec:org913267a"
+    "sec:orgeddf66a"
+    "sec:orga1b69fd"
+    "sec:org53a4076"
+    "sec:org4ae7e40"
+    "sec:orgf6ae6d4"
+    "sec:orgc4fdd6e"
+    "sec:orgaf60ffa"
+    "sec:orgb470dca"
+    "sec:orga32a336"
+    "sec:org4cb7c54"
+    "sec:orgf8d5264"))
  :latex)
 
